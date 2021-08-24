cb155cae6283c51972c55637daef45567d40efcf
Riggs <52392247+RiggsWebDev@users.noreply.github.com>
2021-08-24 14:23:01 -0500

    Add files via upload
    Very simple script to grab double the absolute value of anything higher than 17. Effectively just very basic-level practice without running to Stackoverflow or dissecting someone else's code to make my own.

---
+++ doubleabsolutevalue.py
@@ -0,0 +1,18 @@
+x = int(input("enter a number: "))
+if x < 17:
+    print("Less than 17, you go away now. Zug-Zug")
+elif x > 17:
+    print(abs(x)*2)
+else:
+    quit()
+
+"""
+
+This is a script I created to take the input of a user, and if the number is greater than 17-
+then it prints that value doubled. 
+
+This is a small section of basic Python problems I'm solving for W3. 
+
+I may comment out and save a few of them for later. 
+
+"""
