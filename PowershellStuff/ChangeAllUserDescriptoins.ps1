3f392340df12c72021f4d95ecc399da4738c7380
Riggs <52392247+RiggsWebDev@users.noreply.github.com>
2021-08-26 12:26:04 -0500

    Create ChangeAllUserDescriptoins.ps1
    This is to be used for changing description values of all AD users.

---
+++ PowershellStuff/ChangeAllUserDescriptoins.ps1
@@ -0,0 +1,11 @@
+Import-Module ActiveDirectory
+##########################################
+#                                        #
+#                                        #
+#        Do not uncomment line 8 and 9   #
+#           they are for testing only    #
+#                                        #
+##########################################
+#Set-ADUser -SearchBase "dc=redacted,dc=redacted,dc=redacted,dc=redacted" -Description "Non-Certified" -Filter *
+#Get-AdGroup -Filter * -Searchbase "OU=Staff, OU=User Groups, DC=redacted, DC=redacted, DC=redacted, DC=redacted" | Get-ADGroupMember
+Get-ADUser -Filter * -SearchBase "dc=redacted,dc=redacted,dc=redacted,dc=redacted" | Set-ADUser -Replace @{description="Non-Certified"}
