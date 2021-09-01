77a1374cfbf7cddce4aa9f1d3494069e0122e6b8
Riggs <52392247+RiggsWebDev@users.noreply.github.com>
2021-09-01 11:30:24 -0500

    Update RockPaperScissors.py

--- PythonScripts/Exercises/RockPaperScissors.py
+++ PythonScripts/Exercises/RockPaperScissors.py
@@ -2,22 +2,60 @@ import random
 
 """
 
+
 Welcome to Rock Paper Scissors!
 
 
 """
 
+
+#User chooses what he wants to play, or can choose to quit
 userinput = str(input("Choose (R)ock, (P)aper, (S)cissors, or (Q)uit\n")).lower()
+
+#Random generation choices for the PC
 pcinput = random.randint(1,3)
 if pcinput == 1:
-    pcinput = "r"
+    pcinput = "rock"
 elif pcinput == 2:
-    pcinput = "p"
+    pcinput = "paper"
 elif pcinput == 3:
-    pcinput = "s"
+    pcinput = "scissors"
 
-
+#Declaration of choices
 if userinput == "q":
     quit()
 elif userinput == "r":
+    print("\nYou've chosen rock!")
+elif userinput == "p":
+    print("\nYou've chosen paper!")
+elif userinput == "s":
+    print("\nYou've chosen scissors!")
+else:
+    print("Bye!")
     quit()
+
+print("The computer has chosen",pcinput+"!\n")
+
+#Rock Choices
+if userinput == "r" and pcinput == "rock":
+    print("It's a draw")
+elif userinput == "r" and pcinput == "paper":
+    print("You lose!")
+elif userinput == "r" and pcinput == "scissors":
+    print("You win")
+
+#Paper Choices
+elif userinput == "p" and pcinput == "rock":
+    print("You win")
+elif userinput == "p" and pcinput == "paper":
+    print("It's a draw")
+elif userinput == "p" and pcinput == "scissors":
+    print("You lose!")
+
+#Scissors Choices
+elif userinput == "s" and pcinput == "rock":
+    print("You lose!")
+elif userinput == "s" and pcinput == "paper":
+    print("You win")
+elif userinput == "s" and pcinput == "scissors":
+    print("It's a draw")
