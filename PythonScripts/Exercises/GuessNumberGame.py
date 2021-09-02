8603432e6e46e0ec086d729b7b2fedcb40e11308
Riggs <52392247+RiggsWebDev@users.noreply.github.com>
2021-09-02 12:33:44 -0500

    Update GuessNumberGame.py

--- PythonScripts/Exercises/GuessNumberGame.py
+++ PythonScripts/Exercises/GuessNumberGame.py
@@ -1,9 +1,23 @@
-#Answering the Guess the Number question for Automate the Boring Stuff
-
 import random
-x = 0
-x = random.randint(1,50)
+x = 15
+winner = 0
+
+"""
+
+I danced around the possibility of making the game revolve around two seperate functions, but decided against it after prototyping a little.
+Keeping this here in case I decide to change my mind. 
+
+def makerandom():
+    global x
+    x = 0
+    x = random.randint(1,50)
+    guess()
+"""
+
 def guess():
+    global x
+    global winner
+    print(x)
     y = int(input("Guess a number!\n"))
     if x > y:
         print("You number is too low!")
@@ -12,6 +26,16 @@ def guess():
         print("You number is too high")
         guess()
     elif x == y:
+        winner += 1
+        x = random.randint(1,50)
         print("You win!")
+        print("You've won",winner,"times!")
+        if winner > 5:
+            wn = input("Do you still want to play? (Y)es or (N)o\n").lower()
+            if wn == "y":
+                guess()
+            if wn == "n":
+                quit()
+        guess()
 
 guess()
