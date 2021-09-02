a6ee1d3d8cb273df91abc47166e90b75d008c802
Riggs <52392247+RiggsWebDev@users.noreply.github.com>
2021-09-02 12:49:13 -0500

    Update GuessNumberGame.py
    added comments and optimization

--- PythonScripts/Exercises/GuessNumberGame.py
+++ PythonScripts/Exercises/GuessNumberGame.py
@@ -1,12 +1,12 @@
 import random
+
+#starts with the number 15 they have to pick, afterwards randomizing during each call of guess()
 x = 15
+
+#keeps track of wins
 winner = 0
 
 """
-
-I danced around the possibility of making the game revolve around two seperate functions, but decided against it after prototyping a little.
-Keeping this here in case I decide to change my mind. 
-
 def makerandom():
     global x
     x = 0
@@ -17,7 +17,12 @@ def makerandom():
 def guess():
     global x
     global winner
-    print(x)
+
+
+    #this is for troubleshooting, prints the number it picks. 
+    #print(x)
+
+
     y = int(input("Guess a number!\n"))
     if x > y:
         print("You number is too low!")
@@ -30,7 +35,9 @@ def guess():
         x = random.randint(1,50)
         print("You win!")
         print("You've won",winner,"times!")
-        if winner > 5:
+        
+        #Every 5 wins it asks if you still want to play as to not be completely tedious
+        if winner % 5 == 0:
             wn = input("Do you still want to play? (Y)es or (N)o\n").lower()
             if wn == "y":
                 guess()
